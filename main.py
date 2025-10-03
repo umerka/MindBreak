4692f9fab10decf58a44955af4d3e043e597eec1
SuperPaperFox <mumark1234+git@gmail.com>
2025-10-03 17:36:31 +0500

    "[]" has been changed from loops to "execute if 0" operators

--- main.py
+++ main.py
@@ -10,20 +10,29 @@ for i in range(1000):
     tape.append(0)
 
 def main():
+    skip = False
+
 
     idx = 0
     array = list(readFile("example.mb"))
     for i in array:
-        if i == '>':
+        if i == '>' and skip == False:
             idx+=1
-        if i == '<':
+        if i == '<' and skip == False:
             idx-=1
-        if i == '+':
+        if i == '+' and skip == False:
             tape[idx] += 1
-        if i == ".":
+        if i == "." and skip == False:
             print(chr(tape[idx]))
-        if i == ',':
+        if i == ',' and skip == False:
             tape[idx] = int(input(">"))
+        if i == '[' and skip == False:
+            if tape[idx] != 0:
+                skip = True
+            else:
+                skip = False
+        if i == ']':
+            skip = False
 
 
 if __name__ == "__main__":
